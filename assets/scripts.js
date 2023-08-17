4ef76269e8c9745cb2f64a3292df357e48b639c0
Caleb Mieszko <caleb.mieszko@gmail.com>
2023-08-16 20:37:03 -0700

    Added some comments

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,7 +4,13 @@ let newInventoryYearBuilt = [1922, 2011, 1975, 2002, 1965, 1951]
 let typeS1Count = 0;
 let lorrieCount = 0;
 
-/* let i = 0;
+/* 
+
+// This is commented out because it was just a while loop in order to learn them better, 
+// but I wanted to show you a better way to write more concise code below, 
+// by including both 'if' statements in one loop
+
+let i = 0;
 
 while(i < newInventoryCarModels.length){
     if(newInventoryCarModels[i] === 'S1'){
